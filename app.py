fc524217a77681834c03d906ab3b25efadda3046
jt755A <jot755A@gmail.com>
2026-04-09 17:49:44 -0700

    Thurs 04/09 progress

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import (
     url_for,
 )
 
-from todos.utils import error_for_list_title, find_list_by_id
+from todos.utils import error_for_list_title, find_list_by_id, error_for_todo_title, find_todo_by_id
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
@@ -66,12 +66,41 @@ def get_list(list_id):
 @app.route("/lists/<list_id>/todos", methods=["POST"])
 def create_todo(list_id):
     todo_title = request.form['todo'].strip()
-
-
     lst = find_list_by_id(list_id, session['lists'])
-    lst['todos'].append(todo_title)
+    if not lst:
+        raise NotFound(description="List not found")
 
-    return render_template('list.html', lst=lst)
+    error = error_for_todo_title(todo_title)
+    if error:
+        flash(error, "error")
+        return render_template('list.html', lst=lst)
+
+    lst['todos'].append({
+        'id': str(uuid4()),
+        'title': todo_title,
+        'completed': False,
+    })
+
+    flash("The todo has been created", "success")
+    session.modified = True
+    # return render_template('list.html', lst=lst)
+    return redirect(url_for('get_list', list_id=list_id))
+
+@app.route("/lists/<list_id>/todos/<todo_id>/toggle", methods=["POST"])
+def toggle_todo(list_id, todo_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    todo = find_todo_by_id(todo_id, lst)
+    if not lst:
+        raise NotFound(description="List not found")
+    elif not todo:
+        raise NotFound(description="Todo not found")
+
+    new_completion_status = request.form['completed']
+    todo['completed'] = new_completion_status
+    flash("Todo status was updated", "success")
+    session.modified = True
+
+    return redirect(url_for('get_list', list_id=list_id))
 
 
 if __name__ == "__main__":
--- todos/utils.py
+++ todos/utils.py
@@ -6,5 +6,14 @@ def error_for_list_title(title, lists):
     else:
         return None
 
+def error_for_todo_title(title):
+    if not 1 <= len(title) <= 100:
+        return "The title must be between 1 and 100 characters"
+    else:
+        return None
+
 def find_list_by_id(list_id, lists):
     return next((lst for lst in lists if lst['id'] == list_id), None)
+
+def find_todo_by_id(todo_id, lst):
+    return next((todo for todo in lst['todos'] if todo['id'] == todo_id), None)
